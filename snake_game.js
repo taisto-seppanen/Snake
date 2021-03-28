74a3644957a1b130ae4f696f2f42bbd87a15d6d1
taisto-seppanen <taistopetrseppanen@gmail.com>
2021-03-28 16:59:17 +0300

    refactored

--- snake_game.js
+++ snake_game.js
@@ -1,12 +1,9 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
-// фон игрового поля
 const ground = new Image();
 ground.src = "ground.png";
-// картинка еды
 const foodImg = new Image();
 foodImg.src = "food.png";
-// картинка для проигрыша
 const loseImg = new Image();
 loseImg.src = "lose.png";
 
@@ -19,7 +16,6 @@ let food = {
   y: Math.floor(Math.random() * 18 + 1) * box,
 };
 
-// змейка представляется как массив
 let snake = [];
 snake[0] = {
   x: 10 * box,
@@ -28,29 +24,38 @@ snake[0] = {
 
 document.addEventListener("keydown", direction);
 
-let dir; // направление движения
+let dir;
+var isOnFocus = 0;
 
-// Обработка нажатий клавишь
+let gameFocus = document.getElementById("game");
+gameFocus.addEventListener("mouseover", (event) => {
+  isOnFocus = 1;
+});
+gameFocus.addEventListener("mouseout", (event) => {
+  isOnFocus = 0;
+});
+
 function direction(event) {
-  if (event.keyCode == 37 && dir != "right") {
-    dir = "left";
-  } else if (event.keyCode == 38 && dir != "down") {
-    dir = "up";
-  } else if (event.keyCode == 39 && dir != "left") {
-    dir = "right";
-  } else if (event.keyCode == 40 && dir != "up") {
-    dir = "down";
+  if (isOnFocus) {
+    event.preventDefault();
+    if (event.keyCode == 37 && dir != "right") {
+      dir = "left";
+    } else if (event.keyCode == 38 && dir != "down") {
+      dir = "up";
+    } else if (event.keyCode == 39 && dir != "left") {
+      dir = "right";
+    } else if (event.keyCode == 40 && dir != "up") {
+      dir = "down";
+    }
   }
 }
 
-// проверяет не съеден ли хвост
 function eatTail(head, arr) {
   for (let i = 0; i < arr.length; i++) {
     if (head.x == arr[i].x && head.y == arr[i].y) lose();
   }
 }
 
-// обработка события проигрыша
 function lose() {
   ctx.drawImage(loseImg, 0, 0);
   dieSound.play();
@@ -58,7 +63,6 @@ function lose() {
   ask();
 }
 
-// подключаем озвучку
 function sound(src) {
   this.sound = document.createElement("audio");
   this.sound.src = src;
@@ -76,7 +80,6 @@ function sound(src) {
 const eatSound = new sound("./EatSound.ogg");
 const dieSound = new sound("./DieSound.ogg");
 
-// Методы для перезагрузки игры
 const forceReload = () => {
   clearInterval(game);
   location.reload(true);
@@ -87,7 +90,6 @@ function ask() {
   tryAgain ? forceReload() : clearInterval(game);
 }
 
-// отрисовка игры
 function drawGame() {
   ctx.drawImage(ground, 0, 0);
   ctx.drawImage(foodImg, food.x, food.y);
@@ -104,9 +106,6 @@ function drawGame() {
   let snakeY = snake[0].y;
   let snakeX = snake[0].x;
 
-  console.log("food.x = ", food.x, "food.y = ", food.y);
-
-  // обрабатываем поедание яблок
   if (snakeX == food.x && snakeY == food.y) {
     eatSound.play();
     score++;
@@ -117,13 +116,10 @@ function drawGame() {
   } else {
     snake.pop();
   }
-
-  // Обрабатываем столкновение с стеной
   if (snakeX > box * 19 || snakeX < 0 || snakeY > box * 19 || snakeY == 0) {
     lose();
   }
 
-  // обработка направления движения
   if (dir == "up") snakeY -= box;
   if (dir == "down") snakeY += box;
   if (dir == "left") snakeX -= box;
